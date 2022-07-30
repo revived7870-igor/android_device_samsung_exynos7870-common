0ab3553c720b887f14e0b38415d02dc750f3ade8
A2L5E0X1 <alexed25@protonmail.com>
2022-07-30 19:01:11 +0200

    exynos7870-common: import init files
    * Taken from A600FNXXU9CVB1
    
    Change-Id: Ibdb418ce4162b681fcab57c3ce28591b57c83959

--- exynos7870.mk
+++ exynos7870.mk
@@ -7,5 +7,13 @@
 ## Common Path
 COMMON_PATH := device/samsung/exynos7870-common
 
+# Init
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/init/fstab.samsungexynos7870:$(TARGET_COPY_OUT_RAMDISK)/fstab.samsungexynos7870 \
+    $(COMMON_PATH)/configs/init/fstab.samsungexynos7870:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.samsungexynos7870 \
+    $(COMMON_PATH)/configs/init/init.samsungexynos7870.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.samsungexynos7870.rc \
+    $(COMMON_PATH)/configs/init/init.samsungexynos7870.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsungexynos7870.usb.rc \
+    $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
