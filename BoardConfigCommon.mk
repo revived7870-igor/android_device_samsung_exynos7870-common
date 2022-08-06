74b62b69e6890d18bd02da47e466c6e99c194a13
A2L5E0X1 <alexed25@protonmail.com>
2022-08-06 12:50:01 +0200

    exynos7870-common: set TARGET_LINUX_KERNEL_VERSION
    * required for the bsp
    
    Change-Id: I6f35e5c0ab3126134c8bfeefa7692a00541c39cb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,6 +65,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+TARGET_LINUX_KERNEL_VERSION := 3.18
 
 # Kernel Source
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos7870
