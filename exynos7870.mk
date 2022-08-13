7c4a0a27f5a671c0c2f475aab5d46015b598a103
A2L5E0X1 <alexed25@protonmail.com>
2022-08-13 17:27:22 +0200

    exynos7870-common: import 32 bit sensors service
    Change-Id: I60b66994d0e7da9723edfd7c4a57a57850f19b66

--- exynos7870.mk
+++ exynos7870.mk
@@ -234,7 +234,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service_32
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
