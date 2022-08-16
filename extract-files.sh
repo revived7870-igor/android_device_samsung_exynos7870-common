1d02215b98f8e3073b126a4c604922591300501a
R0rt1z2 <rogerortizleal@gmail.com>
2022-08-16 20:47:22 +0200

    exynos7870-common: proprietary: patch libbauthtzcommon to always return 0 in BAuth_Hat_OP function
    * based on https://github.com/LineageOS/android_device_samsung_a5xelte/commit/6f6590328ffbccc5a67bf3899fee2bc62ad0e09b but in a treble compliant way
    
    Co-authored-by: "Christopher N. Hesse" <raymanfx@gmail.com>
    Change-Id: Ibdcce4e0a9f1f81e2f0601572f0997669d1e3b22

--- extract-files.sh
+++ extract-files.sh
@@ -60,6 +60,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+	vendor/lib/libbauthtzcommon.so)
+	    sed -i "s|\x0b\xd0\xff\xff-\xe9\xf0O\x83\xb0\x89F|\x0b\xd0\xff\xff\x00 pG\x83\xb0\x89F|g" "${2}"
+	    ;;
 #        vendor/lib*/libsec_semRil.so|vendor/lib*/libwrappergps.so)
 #            "${PATCHELF}" --replace-needed "libsecril-client.so" "libsecril-client-exynos7870.so" "${2}"
 #            "${PATCHELF}" --set-soname "libsecril-client-exynos7870.so" "${2}"
