1359f6fda11e5e7f04124a90d50c66bd41ff936c
Igor121 <chernokalsky89@gmail.com>
2023-11-06 15:37:40 +0300

    exynos7870-common: decommonize fstab * a3y17lte lacks a vendor partition as well as odm

--- exynos7870.mk
+++ exynos7870.mk
@@ -106,8 +106,6 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/init/fstab.samsungexynos7870:$(TARGET_COPY_OUT_RAMDISK)/fstab.samsungexynos7870 \
-    $(COMMON_PATH)/configs/init/fstab.samsungexynos7870:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.samsungexynos7870 \
     $(COMMON_PATH)/configs/init/init.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsung.rc \
     $(COMMON_PATH)/configs/init/init.samsungexynos7870.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.samsungexynos7870.rc \
     $(COMMON_PATH)/configs/init/init.samsungexynos7870.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsungexynos7870.usb.rc \
