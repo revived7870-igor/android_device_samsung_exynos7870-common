6acafeb3cf93b212b077324d5dc31109026f85bd
Igor121 <chernokalsky89@gmail.com>
2023-11-10 18:05:18 +0300

    exynos7870-common: decommonize blobs
    * WiFi is different all over the platform
    * Camera stacks differ from device to device (Oreo/Q)
    * sensors are entirely different
    * other stuff i may not remember

--- extract-files.sh
+++ extract-files.sh
@@ -60,10 +60,6 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-	vendor/lib/hw/camera.exynos7870.so|vendor/lib/libexynoscamera.so|vendor/lib/libexynoscamera3.so)
-	    "${PATCHELF}" --replace-needed "libcamera_client.so" "libcamera_metadata_helper.so" "${2}"
-	    "${PATCHELF}" --replace-needed "libgui.so" "libgui_vendor.so" "${2}"
-	    ;;
 	vendor/lib/libbauthtzcommon.so)
 	    sed -i "s|\x0b\xd0\xff\xff-\xe9\xf0O\x83\xb0\x89F|\x0b\xd0\xff\xff\x00 pG\x83\xb0\x89F|g" "${2}"
 	    ;;
