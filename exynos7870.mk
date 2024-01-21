e41436ceb72fe6be2ccac288b1bd17b4700c9cf3
Igor121 <chernokalsky89@gmail.com>
2024-01-21 11:52:02 +0300

    exynos7870-common: drop nonexistent component
    * keystore.exynos7870 never existed in the first place

--- exynos7870.mk
+++ exynos7870.mk
@@ -128,8 +128,7 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-    keystore.exynos7870
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
